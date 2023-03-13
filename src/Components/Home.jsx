4041215dc46ec5b8faf3715f2ee2dc044a64e096
Hany Afifi <hanyafifi@gmail.com>
2023-03-13 21:04:56 +0200

    fix typography

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,51 +3,51 @@ import handyman from "./img/handyman.jpeg";
 
 function Home() {
   return <div className="container">
-    <div className="row">
+    <div className="row mt-5 mx-auto">
 
 
 
 
 
-<div className="row row-cols-1 row-cols-md-3 g-4">
-  <div className="col">
-    <div className="card h-100">
+
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
     <img src={handyman}  className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-      <div className="card-footer">
-        <small className="text-muted">Last updated 3 mins ago</small>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
   </div>
-  <div className="col">
-    <div className="card h-100">
+  <div class="col">
+    <div class="card h-100">
     <img src={handyman}  className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      </div>
-      <div className="card-footer">
-        <small className="text-muted">Last updated 3 mins ago</small>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a short card.</p>
       </div>
     </div>
   </div>
-  <div className="col">
-    <div className="card h-100">
+  <div class="col">
+    <div class="card h-100">
     <img src={handyman}  className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
       </div>
-      <div className="card-footer">
-        <small className="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img src={handyman}  className="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
   </div>
 </div>
-
 
 
 

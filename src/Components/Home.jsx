bd73c753f70099eca65561510319c36a5d257429
Hany Afifi <hanyafifi@gmail.com>
2023-03-13 23:14:24 +0200

    added cards for construction

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import handyman from "./img/handyman.jpeg";
+import painter from "./img/painter.jpeg";
+import parket from "./img/parket.jpeg";
+import paving from "./img/paving.jpeg";
+import plastering from "./img/plastering.jpeg";
+import readywalls from "./img/readywalls.jpeg";
+import renovation from "./img/renovation.jpeg";
+import tiles from "./img/tiles.jpeg";
+import demolition from "./img/demolition.jpeg";
 
 function Home() {
   return (
     <div className="container">
-      <h2 className="border-bottom pb-1 mb-2 mt-5 ">CONSTRUCTION SERVICES</h2>
-      <h2>BAUDIENSTLEISTUNGEN</h2>
-      <div className="row mx-auto">
-        <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4">
+      <div className="row mx-auto  p-1 mb-2 mt-5 ">
+      <h2 className="border-bottom p-1 ">CONSTRUCTION SERVICES</h2>
+      <h4>BAUDIENSTLEISTUNGEN</h4>
+        <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-2">
           <div className="col">
             <div className="card h-100">
-              <img src={handyman} className="card-img-top" alt="..." />
+              <img src={painter} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Painting & Wallpapering</h5>
                 <p className="card-text">Malerarbeiten & Tapezier</p>
@@ -19,7 +26,7 @@ function Home() {
           </div>
           <div className="col">
             <div className="card h-100">
-              <img src={handyman} className="card-img-top" alt="..." />
+              <img src={readywalls} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Drywall</h5>
                 <p className="card-text">Trockenbau</p>
@@ -28,7 +35,7 @@ function Home() {
           </div>
           <div className="col">
             <div className="card h-100">
-              <img src={handyman} className="card-img-top" alt="..." />
+              <img src={plastering} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Plastering</h5>
                 <p className="card-text">Putzarbeiten</p>
@@ -37,7 +44,7 @@ function Home() {
           </div>
           <div className="col">
             <div className="card h-100">
-              <img src={handyman} className="card-img-top" alt="..." />
+              <img src={paving} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Paving</h5>
                 <p className="card-text">Pflasterarbeiten</p>
@@ -46,7 +53,7 @@ function Home() {
           </div>
           <div className="col">
             <div className="card h-100">
-              <img src={handyman} className="card-img-top" alt="..." />
+              <img src={demolition} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Demolition & Gutting</h5>
                 <p className="card-text">Abriss & Entkernung</p>
@@ -55,7 +62,7 @@ function Home() {
           </div>
           <div className="col">
             <div className="card h-100">
-              <img src={handyman} className="card-img-top" alt="..." />
+              <img src={renovation} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Renovation & Refurbishment</h5>
                 <p className="card-text">Renovierung & Sanierung</p>
@@ -64,7 +71,7 @@ function Home() {
           </div>
           <div className="col">
             <div className="card h-100">
-              <img src={handyman} className="card-img-top" alt="..." />
+              <img src={parket} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Parquet & floor layers</h5>
                 <p className="card-text">Parkett & Bodenleger</p>
@@ -73,7 +80,7 @@ function Home() {
           </div>
           <div className="col">
             <div className="card h-100">
-              <img src={handyman} className="card-img-top" alt="..." />
+              <img src={tiles} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Tiles</h5>
                 <p className="card-text">Fliesen</p>
@@ -83,13 +90,13 @@ function Home() {
         </div>
       </div>
 
-      <h2 className="border-bottom pb-1 mb-2 mt-5 ">BUILDING CLEANING</h2>
+      <div className="row mx-auto  pb-1 mb-2 mt-5">
+      <h2 className="border-bottom">BUILDING CLEANING</h2>
       <h3>GEBÄUDEREINIGUNG</h3>
-      <div className="row mx-auto">
-        <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4">
+        <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-2">
           <div className="col">
             <div className="card h-100">
-              <img src={handyman} className="card-img-top" alt="..." />
+              <img src={painter} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Houses cleaning</h5>
                 <p className="card-text">Häuser reinigung</p>
@@ -98,7 +105,7 @@ function Home() {
           </div>
           <div className="col">
             <div className="card h-100">
-              <img src={handyman} className="card-img-top" alt="..." />
+              <img src={painter} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Offices cleaning</h5>
                 <p className="card-text">Büroreinigung</p>
@@ -107,7 +114,7 @@ function Home() {
           </div>
           <div className="col">
             <div className="card h-100">
-              <img src={handyman} className="card-img-top" alt="..." />
+              <img src={painter} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Glass cleaning </h5>
                 <p className="card-text">Glasreinigung</p>

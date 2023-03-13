9fe584313968d3adaf97b8a2e46807fe8efd8a8b
Hany Afifi <hanyafifi@gmail.com>
2023-03-13 03:35:49 +0200

    init

---
+++ src/Components/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import haniscapeLogoDark from "./img/haniscape-logo-dark.svg";
+
+function Header() {
+  return (
+      <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <div className="container">
+          <a className="navbar-brand" href="/#">
+            <img src={haniscapeLogoDark} alt="Haniscape logo" height="32" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#">
+                  Jobs
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#">
+                  Handymen
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+  );
+}
+
+export default Header;
---
+++ src/Components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "./Header";
+import Home from "./Home";
+
+function Main() {
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default Main;

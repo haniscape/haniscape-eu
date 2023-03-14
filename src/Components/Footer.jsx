40ffefb2a6821fdcf0f83f1d72e333aa177e99e3
Hany Afifi <hanyafifi@gmail.com>
2023-03-15 00:21:00 +0200

    tweak

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
 
 
 
-<footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+<footer className="d-flex flex-wrap justify-content-between align-items-center py-3 m-4 border-top">
     <div className="col-md-4 d-flex align-items-center">
       <a href="/" className="mb-3 me-5 mb-md-0 text-decoration-none lh-1">
       <img src={haniscapeLogoMono} alt="Haniscape logo" height="26" />

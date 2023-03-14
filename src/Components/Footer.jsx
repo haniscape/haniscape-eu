71d2680e58ae413345a741ab3b3e28fc2f94c3c5
Hany Afifi <hanyafifi@gmail.com>
2023-03-15 00:10:06 +0200

    added footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import haniscapeLogoMono from "./img/haniscape-logo-mono.svg";
+import { Twitter, Instagram, Facebook } from 'react-bootstrap-icons';
+
+function Footer() {
+  return (
+
+
+
+
+<footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div className="col-md-4 d-flex align-items-center">
+      <a href="/" className="mb-3 me-5 mb-md-0 text-decoration-none lh-1">
+      <img src={haniscapeLogoMono} alt="Haniscape logo" height="26" />
+      </a>
+      <span className="mb-3 mb-md-0 text-muted">© 2023 UAB Damasteksas.</span>
+    </div>
+
+    <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li className="ms-3"><a className="text-muted" href="/#"><Twitter width="26" height="26" /></a></li>
+      <li className="ms-3"><a className="text-muted" href="/#"><Instagram width="26" height="26" /></a></li>
+      <li className="ms-3"><a className="text-muted" href="/#"><Facebook width="26" height="26" /></a></li>
+    
+    </ul>
+  </footer>
+
+
+
+
+
+
+
+  );
+}
+
+export default Footer;
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "./Header";
 import Home from "./Home";
+import Footer from "./Footer";
 
 function Main() {
   return (
     <div className="container-fluid">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }

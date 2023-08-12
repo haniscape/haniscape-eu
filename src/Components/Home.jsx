95b77532a4c1c38ac994a19f53a0ec9d62bf1d1c
Hany Afifi <hanyafifi@gmail.com>
2023-08-12 15:35:51 +0300

    sdfs

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,18 +8,18 @@ function Home() {
     <div className="container">
       <div className="row mx-auto  p-1 mb-2 mt-5 ">
       <h2>Haniscape Solutions</h2>
-      <h5>Web and Mobile Apps Development </h5>
+      <h5>Web and Mobile Apps Development</h5>
       </div>
 
       <div className="row mx-auto  p-1 mb-2 mt-5 ">
-        <h2 className="border-bottom p-1 ">BAUDIENSTLEISTUNGEN</h2>
-        <h4>CONSTRUCTION SERVICES</h4>
+        <h2 className="border-bottom p-1 ">Our Services</h2>
+        <h4>High quality products for all your needs</h4>
         <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-2">
           <div className="col">
             <div className="card h-100">
               <img src={webApp} className="card-img-top" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">Web Application</h5>
+                <h5 className="card-title">Web Application Development</h5>
                 <p className="card-text">Using latest React and Next technologies</p>
               </div>
             </div>
@@ -28,8 +28,8 @@ function Home() {
             <div className="card h-100">
               <img src={readywalls} className="card-img-top" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">Trockenbau</h5>
-                <p className="card-text">Drywall</p>
+                <h5 className="card-title">Web Application Development</h5>
+                <p className="card-text">High Quality React Native development</p>
               </div>
             </div>
           </div>
